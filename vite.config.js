544b34ce91c4bd1d5a81bd94dd57295b15ad2394
yuuza11 <imamjr165@gmail.com>
2025-06-23 23:48:47 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  base: '/build/', // 👈 penting untuk menghindari mixed content
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
 });
